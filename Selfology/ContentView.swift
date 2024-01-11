663dc3081801e72204d0f056e5be85c6bb95c082
Adam Baqaie <ilyas.baqaie@gmail.com>
2024-01-11 16:29:35 +1100

    Initial Commit

---
+++ Selfology/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Selfology
+//
+//  Created by Hamed BAQAIE on 11/1/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Selfology/SelfologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfologyApp.swift
+//  Selfology
+//
+//  Created by Hamed BAQAIE on 11/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct SelfologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
