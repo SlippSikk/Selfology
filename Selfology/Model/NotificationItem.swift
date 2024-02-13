b41753c926ab20b8de500469d6866b05e1712e5c
Adam Baqaie <ilyas.baqaie@gmail.com>
2024-02-13 14:08:10 +1100

    Edited notification row and notificationItem

--- Selfology/Model/NotificationItem.swift
+++ Selfology/Model/NotificationItem.swift
@@ -10,7 +10,20 @@ import Foundation
 struct NotificationItem: Identifiable, Codable {
     var id: UUID = UUID()
     var isOn: Bool
-    var time: String
+    var time: Date
     var taskName: String
     var description: String
+//    var repeatSchedule: RepeatSchedule
+}
+
+
+enum RepeatSchedule: String, Codable, CaseIterable {
+    case everyDay = "Every Day"
+    case everyMonday = "Every Monday"
+    case everyTuesday = "Every Tuesday"
+    case everyWednesday = "Every Wednesday"
+    case everyThursday = "Every Thursday"
+    case everyFriday = "Every Friday"
+    case everySaturday = "Every Saturday"
+    case everySunday = "Every Sunday"
 }
