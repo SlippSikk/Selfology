8558b3f7431d0434dd860da071c0193bd2ce513f
Adam Baqaie <ilyas.baqaie@gmail.com>
2024-02-15 14:44:59 +1100

    Updated notification row by adding description and settings button. Need to still fix animation

--- Selfology/Views/Notification/NotifcationRow.swift
+++ Selfology/Views/Notification/NotifcationRow.swift
@@ -7,8 +7,8 @@
 
 import SwiftUI
 
-struct NotifcationRow: View {
-    @Binding var item: NotificationItem
+struct NotificationRow: View {
+    @State var item: NotificationItem
     @State private var showDescription = false
     
     let customGray = Color(red: 43.0 / 255.0, green: 43.0 / 255.0, blue: 43.0 / 255.0)
@@ -17,78 +17,95 @@ struct NotifcationRow: View {
         VStack {
             ZStack {
                 RoundedRectangle(cornerRadius: 18)
-                    .fill(Color.gray.opacity(0.11))
-                    .shadow(radius: 5)
-
-                HStack {
-                    Spacer()
-                    Toggle(isOn: $item.isOn) {
-                        EmptyView()
+                    .fill(Color.gray.opacity(showDescription ? 0.09 : 0.03))
+                    .shadow(radius: showDescription ? 0 : 5)
+                    .animation(.easeInOut, value: showDescription)
+                
+                VStack {
+                    HStack {
+                        Spacer().frame(width: 7).hidden()
+                        Toggle(isOn: $item.isOn) {
+                            EmptyView()
+                        }
+                        .labelsHidden()
+                        .toggleStyle(SwitchToggleStyle(tint: customGray))
+                        //                    .padding(.leading, 7)
+                        
+                        Spacer(minLength: 25)
+                        
+                        // Time capsule
+                        Text(formatDate(item.time))
+//                            .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
+                            .font(.subheadline)
+                            .frame(width: 80, height: 31)
+                            .background(
+                                RoundedRectangle(cornerRadius: 18)
+                                    .stroke(customGray, lineWidth: 1)
+                            )
+                        
+                        Spacer(minLength: 25)
+                        
+                        // Task name capsule
+                        Text(item.taskName)
+//                            .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10)) // Adjust padding as needed
+                            .frame(width: 160, height: 31)
+                            .font(.subheadline)
+                            .background(
+                                RoundedRectangle(cornerRadius: 18)
+                                    .stroke(customGray, lineWidth: 1)
+                            )
+                        
+                        
+//                        Spacer(min) // Flexible space
+                        
+                        // Invisible spacer to balance the leading padding
+                                            Spacer().frame(width: 7).hidden()
                     }
-                    .labelsHidden()
-                    .toggleStyle(SwitchToggleStyle(tint: customGray))
-//                    .padding(.leading, 7)
-                    
-                    Spacer(minLength: 25)
+                    .padding(EdgeInsets(top: 7, leading: 0, bottom:7, trailing: 0))
 
-                    // Time capsule
-                    Text(formatDate(item.time))
-                        .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))                        
-                        .font(.subheadline)
-                        .frame(width: 80, height: 31)
-                        .background(
-                            RoundedRectangle(cornerRadius: 18)
-                                .stroke(customGray, lineWidth: 1)
-                        )
-                    
-                    Spacer(minLength: 25)
-                    
-                    // Task name capsule
-                    Text(item.taskName)
-                        .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10)) // Adjust padding as needed
-                        .frame(width: 160, height: 31)
-                        .font(.subheadline)
-                        .background(
-                            RoundedRectangle(cornerRadius: 18)
-                                .stroke(customGray, lineWidth: 1)
-                        )
+                    if showDescription {
+                        Text(item.description)
+                            .font(.subheadline)
+                            .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .transition(.move(edge: .bottom).combined(with: .opacity))
+                            
                         
-
-                    Spacer() // Flexible space
-                    
-                    // Invisible spacer to balance the leading padding
-//                    Spacer().frame(width: 7).hidden()
-                        
+                        Button(action: {
+                            // Define the action here.
+                            // Since you want no action for now, you can leave this closure empty.
+                        }) {
+                            Image(systemName: "ellipsis")
+                                .font(.title)
+                                .foregroundColor(.primary)
+                        }
+                        .padding(EdgeInsets(top: 2, leading: 0, bottom: 10, trailing: 10)) // Adjust padding as needed
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
                 }
             }
+            .fixedSize(horizontal: false, vertical: true)
             .contentShape(Rectangle())
             .onTapGesture {
-                withAnimation {
+                withAnimation(.easeInOut) {
                     showDescription.toggle()
                 }
             }
-
-            // Conditionally show the description
-            if showDescription {
-                Text(item.description)
-                    .foregroundColor(.gray)
-                    .transition(.opacity)
-                    .padding([.bottom, .horizontal])
-            }
         }
-        .frame(height: showDescription ? nil : 50)
+        .background(Color.gray.opacity(0.11))
+        .cornerRadius(18)
         .padding(.horizontal)
+//        .animation(.easeInOut, value: showDescription)
     }
-    
+
     private func formatDate(_ date: Date) -> String {
         let formatter = DateFormatter()
         formatter.timeStyle = .short
         return formatter.string(from: date)
     }
-
 }
 
 #Preview {
 
-    NotifcationRow(item: .constant(NotificationItem(isOn: true, time: Date(), taskName: "Meditate", description: "Description here")))
+    NotificationRow(item: (NotificationItem(isOn: true, time: Date(), taskName: "Meditate", description: "Wake Up and Meditate to Free The Soul for today is a bright day worth lving. And we must be thankful to God for blessing us and our families. ")))
 }
