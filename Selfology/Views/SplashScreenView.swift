4f1a1371ade436c59f733df347f5f7e1324a811f
Albert Kim <albertjk.10@gmail.com>
2024-03-15 14:08:48 +1100

    SplashScreen view edited to make it spin at 270 degrees within 1.5s size is 110x110

--- Selfology/Views/SplashScreenView.swift
+++ Selfology/Views/SplashScreenView.swift
@@ -9,27 +9,25 @@ import SwiftUI
 
 struct SplashScreenView: View {
     @ObservedObject var viewModel: AppViewModel // Use the shared ViewModel
-    @State private var size: CGFloat = 1.0
-    @State private var opacity: Double = 1.0
+    @State private var angle: Double = 0
 
     var body: some View {
         ZStack {
-            Color(#colorLiteral(red: 0.89, green: 0.88, blue: 0.87, alpha: 1)) // Hex #e4e2dd
+            Color(#colorLiteral(red: 0.89, green: 0.88, blue: 0.87, alpha: 1)) // Background color
                 .edgesIgnoringSafeArea(.all)
             
             VStack {
-                Image("blacklogo")
+                Image("blacklogo") // Your logo here
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 200, height: 200)
-                    .scaleEffect(size)
-                    .opacity(opacity)
+                    .frame(width: 110, height: 110)
+                    .rotationEffect(Angle(degrees: angle))
                     .onAppear {
-                        withAnimation(.easeIn(duration: 1.2)) {
-                            self.size = 0.9
-                            self.opacity = 0.2
+                        // Use easeOut to slow down the animation over time
+                        withAnimation(Animation.easeOut(duration: 1.5)) {
+                            self.angle = 270
                         }
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.4) {
                             withAnimation {
                                 self.viewModel.showSplashScreen = false
                             }
@@ -37,16 +35,12 @@ struct SplashScreenView: View {
                     }
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
-        //.background(Color.white.edgesIgnoringSafeArea(.all))
         }
     }
 }
 
-
 struct SplashScreenView_Previews: PreviewProvider {
     static var previews: some View {
-        // Create an instance of AppViewModel for the preview
         SplashScreenView(viewModel: AppViewModel())
     }
 }
-
