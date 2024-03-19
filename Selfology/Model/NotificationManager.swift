cb0ca7527d9127dfc108e49642da7898028f033b
Adam Baqaie <ilyas.baqaie@gmail.com>
2024-03-19 15:20:53 +1100

    fixed issue where it doesn't correctly load initial json on app startup first time

--- Selfology/Model/NotificationManager.swift
+++ Selfology/Model/NotificationManager.swift
@@ -25,18 +25,6 @@ class NotificationsManager: ObservableObject {
     
     // Loads all the JSON files
     func loadNotifications() {
-//        guard let file = Bundle.main.url(forResource: "notificationItems.json", withExtension: nil) else {
-//            fatalError("Couldn't find notificationsItems.json in main bundle.")
-//        }
-//        
-//        do {
-//            let data = try Data(contentsOf: file)
-//            let decoder = JSONDecoder()
-//            decoder.dateDecodingStrategy = .iso8601
-//            notifications = try decoder.decode([NotificationItem].self, from: data)
-//        } catch {
-//            fatalError("Couldn't load notificationItems.json from main bundle:\n\(error)")
-//        }
         let fileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("notificationItems.json")
         
         // Check if the file exists in the Document directory (persisted data)
@@ -50,13 +38,14 @@ class NotificationsManager: ObservableObject {
             }
         } else {
             // Load the initial data from the bundle as fallback
-            guard let bundleURL = Bundle.main.url(forResource: "notificationItems", withExtension: "json") else {
+            guard let bundleURL = Bundle.main.url(forResource: "notificationItems.json", withExtension: nil) else {
                 fatalError("Couldn't find notificationItems.json in main bundle.")
             }
             do {
                 let data = try Data(contentsOf: bundleURL)
-                notifications = try JSONDecoder().decode([NotificationItem].self, from: data)
-                // Optionally, save the initial data to Document directory for future use
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                notifications = try decoder.decode([NotificationItem].self, from: data)
                 saveNotificationsToFile()
             } catch {
                 fatalError("Couldn't load notificationItems.json from main bundle: \(error)")
